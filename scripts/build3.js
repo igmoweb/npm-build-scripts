7a2e35e3d98bfd3d1665db11ee0f5b083034a73a
Ignacio Cruz <igmoweb@gmail.com>
2018-07-22 15:01:01 +0200

    Linting

--- scripts/build3.js
+++ scripts/build3.js
@@ -1,8 +1,8 @@
 import { rm, exec } from 'shelljs'
 
-const pkgVersion = process.env.npm_package_version;
+const pkgVersion = process.env.npm_package_version
 
-rm( '-rf',  `./dist/${pkgVersion}` );
-exec( `pug --obj data.json src/index.pug --out dist/${pkgVersion}`);
-exec( `node-sass scss/style.scss | cssmin > dist/${pkgVersion}/style.css`);
-exec( `mustache data.json src/index.mustache | uglifyjs > dist/${pkgVersion}/index.js`);
+rm('-rf', `./dist/${pkgVersion}`)
+exec(`pug --obj data.json src/index.pug --out dist/${pkgVersion}`)
+exec(`node-sass scss/style.scss | cssmin > dist/${pkgVersion}/style.css`)
+exec(`mustache data.json src/index.mustache | uglifyjs > dist/${pkgVersion}/index.js`)
--- scripts/server3.js
+++ scripts/server3.js
@@ -1,6 +1,6 @@
 import { exec } from 'shelljs'
 
-const { npm_package_version: pkgVersion, npm_package_config_port: port } = process.env;
+const { npm_package_version: pkgVersion, npm_package_config_port: port } = process.env
 
-exec( `http-server dist/${pkgVersion} -p ${port}`, { async: true } );
-exec( `opn http://localhost:${port}` );
+exec(`http-server dist/${pkgVersion} -p ${port}`, { async: true })
+exec(`opn http://localhost:${port}`)
